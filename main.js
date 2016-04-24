60821d76a84e11cc6f245fad0e3c1f835e772f34
Aron Sai <aron.sai@gmail.com>
2016-04-24 20:29:05 +0200

    allt

---
+++ main.js
@@ -0,0 +1,44 @@
+var lastScroll = 0;
+
+var menu = ["hem", "isverige", "programmet"];
+
+$(function(){
+
+    partOffsets = {};
+
+    for(item of menu) {
+        partOffsets[item] = $("#"+item).offset().top;
+    }
+
+    $(window).scroll(function(e) {
+        var scroll = $(this).scrollTop();
+
+        var offset = lastScroll - scroll;
+        if(Math.abs(offset) > 200) {
+            lastScroll = scroll;
+            return;
+        }
+        if(offset > 0) {
+            $(".navigation").css("opacity", 1);
+        }
+        else {
+            $(".navigation").css("opacity", 0);
+        }
+
+        lastScroll = scroll;
+    });
+
+    var scroller = function(name) {
+
+        $("#"+name+"-nav").click(function() {
+            offset = partOffsets[name] - $(window).height() * 0.02;
+            window.scroll(0, offset);
+        });
+    };
+
+    scroller("hem");
+    scroller("isverige");
+    scroller("programmet");
+
+
+});
